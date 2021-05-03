05efa1a2b1d117796f8008b9c73683b60c8781ba
thirdiallo <thirdiallo@gmail.com>
2021-05-03 12:49:54 +0000

    add mulptiples modules to structure the project

---
+++ src/app/admin/scategory/scategory.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ScategoryRoutingModule } from './scategory-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ScategoryRoutingModule
+  ]
+})
+export class ScategoryModule { }
