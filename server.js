239d2eeaaae6d797b1ada31e67ce32cc3b4bb155
thirtsoft <thirtsoft@gmail.com>
2021-12-27 03:24:21 +0000

    remove request requirement in server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 //const cors = require('cors');
-const request = require("request");
-
 
 app.use(express.static(__dirname + "/dist/shopmania"));
 
